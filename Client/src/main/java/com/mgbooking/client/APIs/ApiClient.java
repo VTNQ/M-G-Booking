ddd238c6ec1e1a856bae77be764df89d3709a688
tranp6648 <tranp6648@gmail.com>
2024-11-02 11:54:00 +0700

    Update

--- Client/src/main/java/com/mgbooking/client/APIs/ApiClient.java
+++ Client/src/main/java/com/mgbooking/client/APIs/ApiClient.java
@@ -10,16 +10,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import java.util.concurrent.TimeUnit;
 
 public class ApiClient {
-    private static Retrofit retrofit=null;
 
-    public static Retrofit getRetrofit(){
-        Gson gson=new GsonBuilder().setDateFormat("dd/MM/yyyy").create();
-        HttpLoggingInterceptor logging=new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client=new OkHttpClient.Builder().addInterceptor(logging)  .connectTimeout(30, TimeUnit.SECONDS)  // Connection timeout
-                .readTimeout(30, TimeUnit.SECONDS)     // Read timeout
-                .writeTimeout(30, TimeUnit.SECONDS) .build();
-        retrofit=new Retrofit.Builder().baseUrl("http://localhost:8686/api/").addConverterFactory(GsonConverterFactory.create(gson)).client(client).build();
-        return retrofit;
-    }
+    
 }
--- Client/src/main/java/com/mgbooking/client/Controllers/AuthController.java
+++ Client/src/main/java/com/mgbooking/client/Controllers/AuthController.java
@@ -1,4 +1,14 @@
 package com.mgbooking.client.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+@RequestMapping({"","/"})
 public class AuthController {
+@GetMapping("Home")
+    public String Home() {
+return "index";
+}
 }
